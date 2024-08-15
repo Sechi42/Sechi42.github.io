4b6cb44a9ee9005be0b4ec3b299dc225244025f2
Sergio Anaya <choptop12@gmail.com>
2024-08-15 09:22:39 -0600

    Modificaciónes a la pagina

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+// script.js
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
-    alert('Gracias por tu mensaje, Sergio se pondrá en contacto contigo pronto!');
+    alert('Formulario enviado');
 });
+
 
